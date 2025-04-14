e9eb9aa125625bd67c730b92ebc3e268c33e6132
jeyoonee <jae4419@naver.com>
2025-04-14 11:40:39 +0900

    solution

--- leetcode/66.js
+++ leetcode/66.js
@@ -1,14 +1,22 @@
-var isSolution = function (digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] < 9) {
-      digits[i]++;
-      return digits;
-    }
-    digits[i] = 0;
+var addBinary = function (a, b) {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  let result = [];
+
+  while (i >= 0 || j >= 0 || carry) {
+    let digitA = i >= 0 ? parseInt(a[i]) : 0;
+    let digitB = j >= 0 ? parseInt(b[i]) : 0;
+
+    let sum = digitA + digitB + carry;
+    result.unshift(sum % 2);
+    carry = Math.floor(sum / 2);
+
+    i--;
+    j--;
   }
 
-  digits.unshift(1);
-  return digits;
+  return result.join("");
 };
 
-console.log(isSolution([4, 3, 9, 9]));
+console.log(addBinary("1010", "1011"));
